fc6153a4a135578a003146384758572212509e83
Vlad Wild <ya.vlash1@yandex.ru>
2022-07-02 14:59:19 +0300

    formatting and delete webhook

--- src/core/server/startup.ts
+++ src/core/server/startup.ts
@@ -1,20 +1,15 @@
-import * as alt from 'alt-server';
-import './modules/db';
-import { DB } from './modules/db';
-import { WebhookResourceStartedWithDB, WebhookResourceStartedWithDBerr } from './modules/webhook';
+import * as alt from "alt-server";
+import "./modules/db";
+import { DB } from "./modules/db";
 
-alt.on('playerConnect', handlePlayerConnect);
+alt.on("playerConnect", handlePlayerConnect);
 
-DB.initialize()
-	.then((connection) => {
-		WebhookResourceStartedWithDB();
-	})
-	.catch((err) => WebhookResourceStartedWithDBerr(err));
+DB.initialize();
 
 function handlePlayerConnect(player: alt.Player) {
-	alt.log(`[${player.id}] ${player.name} has connected to the server.`);
+    alt.log(`[${player.id}] ${player.name} has connected to the server.`);
 
-	player.model = 'mp_m_freemode_01';
-	player.spawn(36.19486618041992, 859.3850708007812, 197.71343994140625, 0);
-	alt.emitClient(player, 'log:Console', 'alt:V Server - Boilerplate Started');
+    player.model = "mp_m_freemode_01";
+    player.spawn(36.19486618041992, 859.3850708007812, 197.71343994140625, 0);
+    alt.emitClient(player, "log:Console", "alt:V Server - Boilerplate Started");
 }
